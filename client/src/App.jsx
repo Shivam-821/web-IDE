9e03aa70aee8a725d472a50c7f1e5b6f30c34889
Shivam Raj <shi821vam@gmail.com>
2025-11-16 12:59:34 +0000

    resizable part added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
 import Terminal from "./components/Terminal";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col">
-      <div className="min-h-[60vh] flex">
-        <div>File-structrue</div>
-        <div>Editor</div>
-      </div>
-      <div>
-        <Terminal />
-      </div>
-    </div>
+    <>
+      <div className="h-8"></div>
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="rounded-lg border md:min-h-[94vh]"
+      >
+        <ResizablePanel defaultSize={20}>
+          <div className="flex h-[200px] items-center justify-center p-6">
+            <span className="font-semibold">One</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={80}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={70}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Two</span>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={30}>
+              <div>
+                <Terminal />
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </>
   );
 }
 
